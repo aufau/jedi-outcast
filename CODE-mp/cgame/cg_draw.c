5a61cdcf0a99499a9b63daec8d44d6dd66e42637
Witold Piłat <witold.pilat@gmail.com>
2014-10-04 19:30:57 +0200

    Remove Connection interrupted message when playing demo

--- CODE-mp/cgame/cg_draw.c
+++ CODE-mp/cgame/cg_draw.c
@@ -2339,28 +2339,30 @@ static void CG_DrawDisconnect( void ) {
 		return;
 	}
 
-	// draw the phone jack if we are completely past our buffers
-	cmdNum = trap_GetCurrentCmdNumber() - CMD_BACKUP + 1;
-	trap_GetUserCmd( cmdNum, &cmd );
-	if ( cmd.serverTime <= cg.snap->ps.commandTime
-		|| cmd.serverTime > cg.time ) {	// special check for map_restart // bk 0102165 - FIXME
-		return;
+	if (!cg.demoPlayback) {
+		// draw the phone jack if we are completely past our buffers
+		cmdNum = trap_GetCurrentCmdNumber() - CMD_BACKUP + 1;
+		trap_GetUserCmd( cmdNum, &cmd );
+		if ( cmd.serverTime <= cg.snap->ps.commandTime
+		     || cmd.serverTime > cg.time ) {	// special check for map_restart // bk 0102165 - FIXME
+			return;
+		}
+
+		// also add text in center of screen
+		s = CG_GetStripEdString("INGAMETEXT", "CONNECTION_INTERRUPTED"); // s = "Connection Interrupted"; // bk 010215 - FIXME
+		w = CG_DrawStrlen( s ) * BIGCHAR_WIDTH;
+		CG_DrawBigString( 320 - w/2, 100, s, 1.0F);
+
+		// blink the icon
+		if ( ( cg.time >> 9 ) & 1 ) {
+			return;
+		}
+
+		x = 640 - 48;
+		y = 480 - 48;
+
+		CG_DrawPic( x, y, 48, 48, trap_R_RegisterShader("gfx/2d/net.tga" ) );
 	}
-
-	// also add text in center of screen
-	s = CG_GetStripEdString("INGAMETEXT", "CONNECTION_INTERRUPTED"); // s = "Connection Interrupted"; // bk 010215 - FIXME
-	w = CG_DrawStrlen( s ) * BIGCHAR_WIDTH;
-	CG_DrawBigString( 320 - w/2, 100, s, 1.0F);
-
-	// blink the icon
-	if ( ( cg.time >> 9 ) & 1 ) {
-		return;
-	}
-
-	x = 640 - 48;
-	y = 480 - 48;
-
-	CG_DrawPic( x, y, 48, 48, trap_R_RegisterShader("gfx/2d/net.tga" ) );
 }
 
 
