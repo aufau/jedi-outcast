4aa79c2dc1fdc5a049f84c6aab8794c7b65ec77d
Witold Piłat <witold.pilat@gmail.com>
2015-02-02 21:40:33 +0100

    Add minimize command

--- CODE-mp/renderer/tr_init.cpp
+++ CODE-mp/renderer/tr_init.cpp
@@ -1014,6 +1014,7 @@ extern qboolean Sys_LowPhysicalMemory();
 	ri.Cmd_AddCommand( "gfxinfo", GfxInfo_f );
 	ri.Cmd_AddCommand("r_we", R_WorldEffect_f);
 	ri.Cmd_AddCommand( "imagecacheinfo", RE_RegisterImages_Info_f);
+	ri.Cmd_AddCommand( "minimize", GLimp_Minimize );
 #endif
 	ri.Cmd_AddCommand( "modellist", R_Modellist_f );
 	ri.Cmd_AddCommand( "modelist", R_ModeList_f );
@@ -1147,8 +1148,8 @@ void RE_Shutdown( qboolean destroyWindow ) {
 	ri.Printf( PRINT_ALL, "RE_Shutdown( %i )\n", destroyWindow );
 
 	ri.Cmd_RemoveCommand ("modellist");
-	ri.Cmd_RemoveCommand ("screenshotJPEG");
 	ri.Cmd_RemoveCommand ("screenshot");
+	ri.Cmd_RemoveCommand ("screenshot_tga");
 	ri.Cmd_RemoveCommand ("imagelist");
 	ri.Cmd_RemoveCommand ("shaderlist");
 	ri.Cmd_RemoveCommand ("skinlist");
@@ -1158,6 +1159,7 @@ void RE_Shutdown( qboolean destroyWindow ) {
 	ri.Cmd_RemoveCommand ("r_we");
 	ri.Cmd_RemoveCommand ("modelcacheinfo");
 	ri.Cmd_RemoveCommand ("imagecacheinfo");
+	ri.Cmd_RemoveCommand ("minimize");
 #ifndef DEDICATED
 	R_ShutdownFonts();
 	if ( tr.registered ) {
